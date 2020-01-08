ef9a16b97ec5f276de83aa8908cca714056b74c4
Stephanie Fu (Populus Group) <v-stfu@microsoft.com>
2020-01-08 15:54:49 -0500

    add sysargs

--- azureml/call_keras.py
+++ azureml/call_keras.py
@@ -4,10 +4,12 @@ import shutil
 import azureml
 import requests
 import json
+import sys
 
 scoring_uri = 'http://localhost:5050/score'
 headers = {'Content-Type':'application/json'}
-test_data = json.dumps({"url":"https://wamu.org/wp-content/uploads/2019/12/Bei-Bei-trip-to-china-1500x1266.jpg"})
+print(sys.argv[0])
+test_data = json.dumps({"url":sys.argv[1]})
 # test_data = sys+"panda.jpg"
 response = requests.post(scoring_uri, data=test_data, headers=headers)
 
