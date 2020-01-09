9ac265d2fe1da7ec8b904ff1bf9adb6d2759c74a
Zhenbang Chen <zbchen29@gmail.com>
2020-01-09 15:50:30 -0500

    Fix image folder reference in stylesheets

--- frontend/src/SearchPage/ResultBox.tsx
+++ frontend/src/SearchPage/ResultBox.tsx
@@ -26,26 +26,6 @@ export default class ResultBox extends Component<IProps> {
     this.state = {};
   }
 
-  //Material UI version
-  // render() {    
-  //   return (
-  //     <div className="grid-card">
-  //       <a className="grid-card__link" href={this.props.data.Link_Resource} target="_blank" rel="noopener noreferrer">
-  //       <LazyLoad
-  //         throttle={250}
-  //         height={500}
-  //         offset={100}
-  //         placeholder={<CircularProgress style={{ color: '#6A6A6A' }} />}
-  //       >
-  //         <img className="grid-card__img" alt="met search result" src={this.props.data.Thumbnail_Url}/>
-  //       </LazyLoad>
-  //       </a>
-  //       <p className="grid-card__title">{this.props.data.Title}</p>
-  //       <p className="grid-card__text">{this.props.data.Department}</p>
-  //     </div>
-  //   );
-  // }
-
   render() {    
     return (
       <Card className="grid-card">
