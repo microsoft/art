aa2de1078f82458b7be655d5f5912c8b266e8b83
Johnny Bui <typngeek@aim.com>
2020-01-23 17:34:41 -0500

    update scoring and deployment script

--- azureml/deploy.py
+++ azureml/deploy.py
@@ -1,30 +1,18 @@
-import azureml, json, os, requests, shutil, urllib
+import azureml, os, time
 
-from azureml.core import Datastore, Experiment, Run, ScriptRunConfig, Workspace
-from azureml.core.compute import AksCompute, AmlCompute, ComputeTarget
+from azureml.core import Workspace
+from azureml.core.compute import AksCompute, ComputeTarget
 from azureml.core.compute_target import ComputeTargetException
-from azureml.core.conda_dependencies import CondaDependencies
 from azureml.core.environment import Environment, DEFAULT_GPU_IMAGE
 from azureml.core.model import InferenceConfig, Model
-from azureml.core.runconfig import RunConfiguration
 from azureml.core.webservice import AksWebservice
 from azureml.exceptions import WebserviceException
-from azureml.train.dnn import TensorFlow
-from keras.applications.imagenet_utils import decode_predictions, preprocess_input
-from keras.applications.resnet50 import ResNet50
-from keras.models import load_model
-from keras.preprocessing import image
 
 ws = Workspace(
     subscription_id="ce1dee05-8cf6-4ad6-990a-9c80868800ba",
     resource_group="extern2020",
     workspace_name="exten-amls"
 )
-
-model = Model.register(
-    model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"my_model.h5"),
-    model_name = "resNet50",
-    workspace = ws)
 
 myenv = Environment.from_conda_specification(
     name="myenv", 
@@ -37,7 +25,7 @@ inference_config = InferenceConfig(
     environment=myenv)
 
 resource_group = 'extern2020'
-cluster_name = 'aks-gpu-new'
+cluster_name = 'aks-gpu2'
 service_name = 'artgpuservice'
 
 """
@@ -46,31 +34,31 @@ Deploys a service to the cluster if one by the name of service_name does not alr
 """
 try: #if cluster and service exists
     aks_target = AksCompute(ws, cluster_name)
-    print("Updating existing service...")
     service = AksWebservice(name=service_name, workspace=ws)
-    service.update(models=[model], inference_config=inference_config)
-    service.wait_for_deployment(show_output=True)
+    print("Updating existing service: {}".format(service_name))
+    # backoff_time = 60
+    # while service.state is "Unschedulable":
+    #     print("Service state is unschedulable. Retrying in {} seconds...".format(backoff_time))
+    #     time.wait(backoff_time)
+    #     backoff_time *= 2 # exponential backoff on unschedulable failure
+    service.update(inference_config=inference_config, auth_enabled=False)
+    # service.wait_for_deployment(show_output=True)
 
 except WebserviceException: #if cluster but no service
-    print("Deploying new service...")
     #creating a new service
-    attach_config = AksCompute.attach_configuration(
-        resource_group = resource_group,
-        cluster_name = cluster_name,
-        cluster_purpose = AksCompute.ClusterPurpose.DEV_TEST)
     aks_target = AksCompute(ws, cluster_name)
-
+    print("Deploying new service: {}".format(service_name))
     gpu_aks_config = AksWebservice.deploy_configuration(
         autoscale_enabled=False,
         num_replicas=3,
         cpu_cores=2,
         memory_gb=4,
         auth_enabled=False)
-    service = Model.deploy(ws, service_name, [model], inference_config, gpu_aks_config, aks_target, overwrite=True)
+    service = Model.deploy(ws, service_name, [], inference_config, gpu_aks_config, aks_target, overwrite=True)
     service.wait_for_deployment(show_output = True)
 
 except ComputeTargetException: #cluster doesn't exist
-    print("Creating new cluster...")
+    print("Creating new cluster: {}".format(cluster_name))
     # Provision AKS cluster with GPU machine
     prov_config = AksCompute.provisioning_configuration(
         vm_size="Standard_NC6",
@@ -82,15 +70,15 @@ except ComputeTargetException: #cluster doesn't exist
     )
     aks_target.wait_for_completion(show_output=True)
 
-    print("Deploying new service...")
+    print("Deploying new service: {}".format(service_name))
     gpu_aks_config = AksWebservice.deploy_configuration(
         autoscale_enabled=False,
         num_replicas=3,
         cpu_cores=2,
         memory_gb=4,
         auth_enabled=False)
-    service = Model.deploy(ws, service_name, [model], inference_config, gpu_aks_config, aks_target, overwrite=True)
+    service = Model.deploy(ws, service_name, [], inference_config, gpu_aks_config, aks_target, overwrite=True)
     service.wait_for_deployment(show_output = True)
 
-print(service.state)
-print("scoring URI: " + service.scoring_uri)
+print("State: " + service.state)
+print("Scoring URI: " + service.scoring_uri)
--- azureml/score.py
+++ azureml/score.py
@@ -1,8 +1,6 @@
 import pickle
 import json
 import numpy as np
-from sklearn.externals import joblib
-from sklearn.linear_model import Ridge
 from azureml.core.model import Model
 
 import requests
@@ -12,32 +10,73 @@ from io import BytesIO
 from inference_schema.schema_decorators import input_schema, output_schema
 from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
 
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input
 from keras.applications.resnet50 import ResNet50
+
+from ConditionalBallTree import ConditionalBallTree
 
 
 def init():
-    global model
+    global culture_model
+    global classification_model
+    global metadata
+    global keras_model
+
+    # downloading java dependencies
+    spark = SparkSession.builder \
+        .master("local[*]") \
+        .appName("TestConditionalBallTree") \
+        .config("spark.jars.packages", "com.microsoft.ml.spark:mmlspark_2.11:1.0.0-rc1-38-cf48d53c-SNAPSHOT") \
+        .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven") \
+        .config("spark.executor.heartbeatInterval", "60s") \
+        .getOrCreate()
+
     #initialize the model architecture and load in imagenet weights
-    model_path = Model.get_model_path('resNet50')
-    model = load_model(model_path)
+    features_culture_path = Model.get_model_path('features_culture')
+    features_classification_path = Model.get_model_path('features_classification')
+    metadata_path = Model.get_model_path('metadata')
+    
+    culture_model = ConditionalBallTree().load(features_culture_path)
+    classification_model = ConditionalBallTree().load(features_classification_path)
+    metadata = pickle.load(open(metadata_path, 'rb'))
+
+    # model for featurizing
+    keras_model = ResNet50(
+        input_shape=[img_width, img_height, 3],
+        weights='imagenet',
+        include_top=False,
+        pooling='avg'
+    )
 
 input_sample = "https://wamu.org/wp-content/uploads/2019/12/Bei-Bei-trip-to-china-1500x1266.jpg"
 output_sample = "giant_panda"
 
 @input_schema('url', StandardPythonParameterType(input_sample))
+@input_schema('n', StandardPythonParameterType('5'))
+@input_schema('param', StandardPythonParameterType('French'))
+@input_schema('paramtype', StandardPythonParameterType('culture'))
 @output_schema( StandardPythonParameterType(output_sample))
-def run(url):
+def run(url, n, paramtype, param):
     try:
         response = requests.get(url) #URL -> response
-        img = Image.open(BytesIO(response.content)).resize((224, 224)) #response -> PIL 
-        np_img = image.img_to_array(img) #PIL -> numpy
-        img_batch = np.expand_dims(np_img, axis=0) #numpy -> batch
-        processed_image = preprocess_input(img_batch, mode='caffe') #pre-process img
-        preds = model.predict(processed_image) #make prediction on img
-        pred_class = decode_predictions(preds, top=1) #decode predition
-        return pred_class[0][0][1] #return the prediction
+        img = Image.open(BytesIO(response.content)).resize((225, 225)) #response -> PIL 
+        # non RGB images won't have the right number of channels
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        img = np.array(img) #PIL -> numpy
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img.astype(np.float))
+
+        features = keras_model.predict(img) # featurize
+        features /= np.linalg.norm(features)
+        img_feature = features[0]
+
+        if paramtype == 'culture':
+            result = culture_model.findMaximumInnerProducts(img_feature, {param}, n)
+        else:
+            result = classification_model.findMaximumInnerProducts(img_feature, {param}, n)
+        # find actual info
+        resultmetadata = [metadata[r[0]] for r in result] # list of metadata: museum, id, url, culture, classification
+        return resultmetadata
     except Exception as err:
         return str(err)
