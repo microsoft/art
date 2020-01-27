49c1776a844d85d0d389f33d6398ebc00cbeefc6
Johnny Bui <typngeek@aim.com>
2020-01-27 10:34:09 -0500

    fix featurize deploy on reuse of cluster

--- azureml/deploy_featurize.py
+++ azureml/deploy_featurize.py
@@ -22,19 +22,22 @@ datastore = Datastore.register_azure_blob_container(
     sas_token="?sv=2019-02-02&ss=bf&srt=sco&sp=rlc&se=2030-01-23T04:14:29Z&st=2020-01-22T20:14:29Z&spr=https,http&sig=nPlKziG9ppu4Vt5b6G%2BW1JkxHYZ1dlm39mO2fMZlET4%3D",
     create_if_not_exists=True)
 
-cpu_cluster_name = "training"
+cluster_name = "training"
 try:
-    cpu_cluster = ComputeTarget(ws, cpu_cluster_name)
+    # connecting to pre-existing cluster
+    compute_target = ComputeTarget(ws, cluster_name)
     print("Found existing cluster...")
 except:
+    # create a new cluster to train on
     provisioning_config = AmlComputeProvisioningConfiguration(
         vm_size = "Standard_NC6",
         min_nodes = 0,
         max_nodes = 1
     )
-    compute_target = ComputeTarget.create(ws, cpu_cluster_name, provisioning_config)
+    compute_target = ComputeTarget.create(ws, cluster_name, provisioning_config)
 compute_target.wait_for_completion(show_output=True)
 
+# create and run the experiment
 exp = Experiment(workspace=ws, name='featurize_artwork')
 
 estimator = Estimator(
@@ -52,6 +55,7 @@ estimator = Estimator(
 run = exp.submit(estimator)
 run.wait_for_completion(show_output = True)
 
+# save the balltrees made in score.py and metadata
 run.register_model(
     model_name="features",
     model_path="outputs/"
