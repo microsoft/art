bef1dfe2018b316c59ada6f2854ab20895178e63
Johnny Bui <typngeek@aim.com>
2020-01-28 14:52:00 -0500

    initial sharing backend

--- backend/application.py
+++ backend/application.py
@@ -1,63 +1,60 @@
 from flask import Flask, redirect, url_for, request, render_template, jsonify, abort
 from werkzeug.utils import secure_filename
 
-from curated_list import curated_list
-from exploredata import exploredata
+import os
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install("azure-storage-blob")
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+blob_service_client = BlobServiceClient(
+    account_url=connect_str
+)
+container_name = "mosaic-shares"
 
 BAD_REQUEST_STATUS_CODE = 400
 NOT_FOUND_STATUS_CODE = 404
 
 app = Flask(__name__)
 
-default_artwork = {
-    'img_url': "https://lh3.googleusercontent.com/J-mxAE7CPu-DXIOx4QKBtb0GC4ud37da1QK7CzbTIDswmvZHXhLm4Tv2-1H3iBXJWAW_bHm7dMl3j5wv_XiWAg55VOM=s0", 
-    'title': "De Nachtwacht", 
-    'museum': "Rijksmuseum"
-    }
-
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({ "status": "ok", "version": "1.0.0" })
 
-@app.route('/explore', methods=['GET'])
-def explore():
-    for param in ['id', 'museum', 'numResults']:
-        if param not in request.args:
-            jsonify({ "error": "id, museum, and numResults parameter is required" }), BAD_REQUEST_STATUS_CODE
-    
-    try:
-        numResults = int(request.args['numResults'])
-    except:
-        return jsonify({ "error": "numResults must be an integer" }), BAD_REQUEST_STATUS_CODE
-
-    if numResults >= len(exploredata):
-        return jsonify(exploredata)
+@app.route('/upload', methods=['POST'])
+def upload():
+    # frontend uploads image, we save to azure storage blob and return a link to the image and the share page
+    if request.method == 'POST':
+        if request.form.get('image'):
+            upload_file_path = './images/test.txt'
+            file = open('./images','w')
+            file.write(request.form.get('image'))
+            file.close()
+            blob_client = blob_service_client.get_blob_client(container=container_name,blob=upload_file_path)
+            with open(upload_file_path,"rb") as data:
+                blob_client.upload_blob(data)
+        else:
+            return jsonify({"error": "img not found"})
     else:
-        return jsonify(exploredata[:numResults])
+        return jsonify({"error": "upload is a post request"})
 
-@app.route('/select', methods=['GET'])
-def select():
-    if 'id' in request.args and 'museum' in request.args:
-        for art in exploredata:
-            if art['id'] == request.args['id'] and art['museum'] == request.args['museum']:
-                return jsonify(art)
-        
-        return jsonify({ "error": "art object with given id and museum not found" }), NOT_FOUND_STATUS_CODE
-
-    else:
-        return jsonify({ "error": "id and museum parameter is required" }), BAD_REQUEST_STATUS_CODE
-
-
-@app.route('/search', methods=['GET'])
-def search():
-    if 'query' in request.args:
-        return jsonify(exploredata)
-    else:
-        return jsonify({ "error": "query parameter is required" }), BAD_REQUEST_STATUS_CODE
-
-@app.route('/curated', methods=['GET'])
-def curated():
-    return jsonify(curated_list)
+@app.route('/share', methods=['GET'])
+def share():
+    image_url = request.args.get('image_url')
+    title = request.args.get('title')
+    description = request.args.get('description')
+    # input param 'url', we return a page that can be shared with facebook (with correct opengraph tags)
+    return render_template(
+        "share.html",
+        image_url=image_url,
+        title=title,
+        description=description
+    )
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
