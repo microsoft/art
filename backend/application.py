19956ac352eafaf6f92e98d6e21ee74e7655b611
Johnny Bui <typngeek@aim.com>
2020-01-29 10:33:19 -0500

    update sharing backend

--- backend/application.py
+++ backend/application.py
@@ -1,9 +1,11 @@
 from flask import Flask, redirect, url_for, request, render_template, jsonify, abort
 from werkzeug.utils import secure_filename
+import base64
 
 import os
 import subprocess
 import sys
+from io import BytesIO
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
@@ -20,6 +22,10 @@ container_name = "mosaic-shares"
 BAD_REQUEST_STATUS_CODE = 400
 NOT_FOUND_STATUS_CODE = 404
 
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -30,16 +36,24 @@ def home():
 def upload():
     # frontend uploads image, we save to azure storage blob and return a link to the image and the share page
     if request.method == 'POST':
-        if request.form.get('image'):
-            upload_file_path = './images/test.txt'
-            file = open('./images','w')
-            file.write(request.form.get('image'))
-            file.close()
-            blob_client = blob_service_client.get_blob_client(container=container_name,blob=upload_file_path)
-            with open(upload_file_path,"rb") as data:
-                blob_client.upload_blob(data)
-        else:
-            return jsonify({"error": "img not found"})
+        if request.args.get("filename") is None:
+            return jsonify({ "error": "filename parameter must be specified" })
+        filename = request.args.get("filename")
+        try:
+            img_b64 = request.form.get('image').split(',')
+            image = base64.b64decode(img_b64[1])
+        except:
+            return jsonify({ "error": "unable to decode"})
+
+        if allowed_file(filename):
+            filename = secure_filename(filename)
+            blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
+            try:
+                blob_client.upload_blob(image)
+            finally:
+                img_url = "https://mmlsparkdemo.blob.core.windows.net/mosaic-shares/mosaic-shares/" + filename
+                return jsonify({ "img_url": img_url })
+        return jsonify({"error": "error processing file"})
     else:
         return jsonify({"error": "upload is a post request"})
 
@@ -48,12 +62,14 @@ def share():
     image_url = request.args.get('image_url')
     title = request.args.get('title')
     description = request.args.get('description')
+    redirect_url = request.args.get('redirect_url')
     # input param 'url', we return a page that can be shared with facebook (with correct opengraph tags)
     return render_template(
         "share.html",
         image_url=image_url,
         title=title,
-        description=description
+        description=description,
+        redirect_url=redirect_url
     )
 
 if __name__ == "__main__":
