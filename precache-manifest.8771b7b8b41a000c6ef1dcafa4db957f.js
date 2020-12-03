5dce619d47222b3831fd6e9e5c246db5f76f78b1
Mark Hamilton <marhamil@microsoft.com>
2020-12-02 20:37:32 -0500

    Updates

---
+++ precache-manifest.8771b7b8b41a000c6ef1dcafa4db957f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbae00aa3716b4962f7a375a4100bbc",
+    "url": "/art/index.html"
+  },
+  {
+    "revision": "e57338ec5937e37f411d",
+    "url": "/art/static/css/main.bb9337f2.chunk.css"
+  },
+  {
+    "revision": "3ffd044955e214bfffb3",
+    "url": "/art/static/js/2.8701187c.chunk.js"
+  },
+  {
+    "revision": "f943e85d8ec5f7920ed330f4bc1aa9a2",
+    "url": "/art/static/js/2.8701187c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57338ec5937e37f411d",
+    "url": "/art/static/js/main.8ec0cfc7.chunk.js"
+  },
+  {
+    "revision": "adefb41194f5c80540b9",
+    "url": "/art/static/js/runtime-main.e272d18e.js"
+  },
+  {
+    "revision": "b687fe02dda79e66206775a43353c9f0",
+    "url": "/art/static/media/banner5.b687fe02.jpg"
+  },
+  {
+    "revision": "22ffa9db8afba7e106b3c9ddd0561612",
+    "url": "/art/static/media/icon-chevron-left-inverted.22ffa9db.svg"
+  },
+  {
+    "revision": "b372fc5c23219157fb0c14efb6952b96",
+    "url": "/art/static/media/icon-chevron-left.b372fc5c.svg"
+  },
+  {
+    "revision": "6f8497d0415536ef69c1e467f1b785f8",
+    "url": "/art/static/media/icon-chevron-right-inverted.6f8497d0.svg"
+  },
+  {
+    "revision": "14d20b3f358df1e1786aaa0a61ccc238",
+    "url": "/art/static/media/icon-chevron-right.14d20b3f.svg"
+  },
+  {
+    "revision": "49c4642a07d2ec23e3ba346f8b186bd8",
+    "url": "/art/static/media/icon-search.49c4642a.svg"
+  },
+  {
+    "revision": "7ad32ebab6ce6014229066b3846993a0",
+    "url": "/art/static/media/mosaicLogo.7ad32eba.png"
+  },
+  {
+    "revision": "bcf589a203513cc7bda29a69dd83dfbb",
+    "url": "/art/static/media/the_met_logo_crop.bcf589a2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be05bb2be05fac370b2f48854f0782c5.js"
+  "/art/precache-manifest.8771b7b8b41a000c6ef1dcafa4db957f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/art/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
